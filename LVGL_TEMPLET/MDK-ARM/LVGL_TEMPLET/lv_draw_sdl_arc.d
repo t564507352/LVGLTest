c7bb2dff34f38ffb320d5ae678e1dd9951d141a2
bentong <564507352@qq.com>
2024-05-15 20:04:14 +0800

    1

--- LVGL_TEMPLET/MDK-ARM/LVGL_TEMPLET/lv_draw_sdl_arc.d
+++
@@ -1,5 +0,0 @@
-lvgl_templet\lv_draw_sdl_arc.o: ..\Middlewares\LVGL\GUI\lvgl\src\draw\sdl\lv_draw_sdl_arc.c
-lvgl_templet\lv_draw_sdl_arc.o: ..\Middlewares\LVGL\GUI\lvgl\src\draw\sdl\../../lv_conf_internal.h
-lvgl_templet\lv_draw_sdl_arc.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
-lvgl_templet\lv_draw_sdl_arc.o: ..\Middlewares\LVGL\GUI\lvgl\src\draw\sdl\../../lv_conf_kconfig.h
-lvgl_templet\lv_draw_sdl_arc.o: ../Middlewares/LVGL/GUI/lvgl/examples/porting/../../lv_conf.h
--- LVGL_TEMPLET/MDK-ARM/LVGL_TEMPLET/tjpgd.d
+++
@@ -1,6 +0,0 @@
-lvgl_templet\tjpgd.o: ..\Middlewares\LVGL\GUI\lvgl\src\extra\libs\sjpg\tjpgd.c
-lvgl_templet\tjpgd.o: ..\Middlewares\LVGL\GUI\lvgl\src\extra\libs\sjpg\tjpgd.h
-lvgl_templet\tjpgd.o: ..\Middlewares\LVGL\GUI\lvgl\src\extra\libs\sjpg\../../../lv_conf_internal.h
-lvgl_templet\tjpgd.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
-lvgl_templet\tjpgd.o: ..\Middlewares\LVGL\GUI\lvgl\src\extra\libs\sjpg\../../../lv_conf_kconfig.h
-lvgl_templet\tjpgd.o: ../Middlewares/LVGL/GUI/lvgl/examples/porting/../../lv_conf.h
